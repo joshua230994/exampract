0ae8811c5b2c1db13b4cee4090896fc5195a11e7
Bryan Morgado <bryan.morgado@Seguritech.local>
2019-05-31 16:04:24 -0500

    primer subida

---
+++ examenpractico/src/main/java/com/training/finalexam/examenpractico/ExamenpracticoApplication.java
@@ -0,0 +1,13 @@
+package com.training.finalexam.examenpractico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenpracticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenpracticoApplication.class, args);
+	}
+
+}
